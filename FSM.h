264db1ce6a27020bbd483fec6ebe013b6c8aad7a
Callum McColl <callum.mccoll1@gmail.com>
2015-11-07 12:01:02 +1000

    Renamed Swift_FSM framework to FSM.  Moved all FSM related files into the appropriate directory

---
+++ FSM.h
@@ -0,0 +1,19 @@
+//
+//  FSM.h
+//  FSM
+//
+//  Created by Callum McColl on 7/11/2015.
+//  Copyright © 2015 MiPal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FSM.
+FOUNDATION_EXPORT double FSMVersionNumber;
+
+//! Project version string for FSM.
+FOUNDATION_EXPORT const unsigned char FSMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FSM/PublicHeader.h>
+
+
