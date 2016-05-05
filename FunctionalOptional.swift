8761f9ee806af8a79660c29cff2f0fe1b0fdebb1
Callum McColl <callum.mccoll1@gmail.com>
2016-05-06 01:48:08 +1000

    Moved the @noescape in front of the parameter name to infront of the type signature in FunctionalOptional and FunctionalSequence

--- FunctionalOptional.swift
+++ FunctionalOptional.swift
@@ -39,7 +39,7 @@
  *
  * - returns: A value of type `Optional<U>`
  */
-public func <^> <T, U>(@noescape f: T -> U, a: T?) -> U? {
+public func <^> <T, U>(f: @noescape T -> U, a: T?) -> U? {
     return a.map(f)
 }
 
@@ -69,7 +69,7 @@ public func <*> <T, U>(f: (T -> U)?, a: T?) -> U? {
  *
  * - returns: A value of type `Optional<U>`
  */
-public func >>- <T, U>(a: T?, @noescape f: T -> U?) -> U? {
+public func >>- <T, U>(a: T?, f: @noescape T -> U?) -> U? {
     return a.flatMap(f)
 }
 
@@ -84,7 +84,7 @@ public func >>- <T, U>(a: T?, @noescape f: T -> U?) -> U? {
  *
  * - returns: A value of type `Optional<U>`
  */
-public func -<< <T, U>(@noescape f: T -> U?, a: T?) -> U? {
+public func -<< <T, U>(f: @noescape T -> U?, a: T?) -> U? {
     return a.flatMap(f)
 }
 
