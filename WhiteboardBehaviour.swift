752a24bbd7b3e139301c5cd9599d2d0f0582e369
Callum McColl <callum.mccoll1@gmail.com>
2016-05-24 15:20:20 +1000

    Added checking if t is 0 before performing mod in WhiteboardBehaviour

--- WhiteboardBehaviour.swift
+++ WhiteboardBehaviour.swift
@@ -75,7 +75,7 @@ public func trigger<T: GlobalVariables>(
             return nil
         }
         // Wrap t around if it goes above eventCount's max value.
-        t = t % Time(wb.eventCount.dynamicType.max)
+        t = Time.min + (0 == t ? 0 : t % Time(wb.eventCount.dynamicType.max))
         // Calculate the minimum time allowed.
         let eventCount: Time = Time(wb.eventCount)
         let generations: Time = Time(wb.generations)
