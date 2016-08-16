8ddcd047293dc7a321e1baefab0adeb4dcf1330e
Callum McColl <callum.mccoll1@gmail.com>
2016-08-17 02:35:10 +1000

    Removed typealias in MiPalRinglet

--- MiPalRinglet.swift
+++ MiPalRinglet.swift
@@ -66,8 +66,6 @@
  */
 public class MiPalRinglet<S: Snapshotable>: Ringlet {
 
-    public typealias _StateType = MiPalState
-
     internal var globals: S
 
     internal var previousState: MiPalState
