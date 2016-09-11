b91e169c16e60bcb94917a3c66a5c242f6f2306a
Callum McColl <callum.mccoll1@gmail.com>
2016-09-11 18:30:48 +1000

    Upgraded FunctionalOperators.swift to swift-3.0-GM-Seed

--- FunctionalOperators.swift
+++ FunctionalOperators.swift
@@ -28,57 +28,37 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+precedencegroup FunctionalPrecedence {
+    associativity: left
+}
+
 /**
  *  Map a function over a value with context.
  *
  *  Expected function type: `(a -> b) -> f a -> f b`.
  */
-infix operator <^> {
-    associativity left
-
-    // Same precedence as the equality operator (`==`).
-    precedence 130
-}
+infix operator <^> : FunctionalPrecedence
 
 /**
  *  Apply a function with context to a value with context.
  *
  *  Expected function type: `f (a -> b) -> f a -> f b`.
  */
-infix operator <*> {
-    associativity left
-
-    // Same precedence as the equality operator (`==`).
-    precedence 130
-}
+infix operator <*> : FunctionalPrecedence
 
 /**
  *  Map a function over a value with context and flatten the result.
  *
  *  Expected function type: `m a -> (a -> m b) -> m b`.
  */
-infix operator >>- {
-    associativity left
-
-    // Lower precedence than the logical comparison operators
-    // (`&&` and `||`), but higher precedence than the assignment
-    // operator (`=`).
-    precedence 100
-}
+infix operator >>- : FunctionalPrecedence
 
 /**
  *  Map a function over a value with context and flatten the result.
  *
  *  Expected function type: `(a -> m b) -> m a -> m b`.
  */
-infix operator -<< {
-    associativity right
-
-    // Lower precedence than the logical comparison operators
-    // (`&&` and `||`), but higher precedence than the assignment
-    // operator (`=`).
-    precedence 100
-}
+infix operator -<< : FunctionalPrecedence
 
 /**
  *  Compose two functions that produce results in a context, from left to right,
@@ -86,12 +66,7 @@ infix operator -<< {
  *
  *  Expected function type: `(a -> m b) -> (b -> m c) -> a -> m c`.
  */
-infix operator >-> {
-    associativity right
-
-    // Same precedence as `>>-` and `-<<`.
-    precedence 100
-}
+infix operator >-> : FunctionalPrecedence
 
 /**
  *  Compose two functions that produce results in a context, from right to left,
@@ -101,9 +76,4 @@ infix operator >-> {
  *
  *  Expected function type: `(b -> m c) -> (a -> m b) -> a -> m c`.
  */
-infix operator <-< {
-    associativity right
-
-    // Same precedence as `>>-` and `-<<`.
-    precedence 100
-}
+infix operator <-< : FunctionalPrecedence
