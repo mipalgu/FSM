88883ada0b80d71ed78c5cad6ad39f639c73966c
Callum McColl <callum.mccoll1@gmail.com>
2016-09-30 05:15:36 +1000

    Added init to KripkeStructure

--- KripkeStructure.swift
+++ KripkeStructure.swift
@@ -59,5 +59,9 @@
 public struct KripkeStructure: KripkeStructureType {
     
     public let states: [KripkeState]
+
+    public init(states: [KripkeState] = []) {
+        self.states = states
+    }
     
 }
