1c818fbf07ee631432c4433a8273ecb99f9583c6
Callum McColl <callum.mccoll1@gmail.com>
2017-02-08 13:38:11 +1000

    Create module map for FSM

---
+++ FSM/FSM.h
@@ -0,0 +1,20 @@
+//
+//  FSM.h
+//  FSM
+//
+//  Created by Callum McColl on 22/1/17.
+//  Copyright © 2017 Callum McColl. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#include <gusimplewhiteboard/gusimplewhiteboard.h>
+
+//! Project version number for FSM.
+FOUNDATION_EXPORT double FSMVersionNumber;
+
+//! Project version string for FSM.
+FOUNDATION_EXPORT const unsigned char FSMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FSM/PublicHeader.h>
+
+
