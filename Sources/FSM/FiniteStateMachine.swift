950f3e4b2fcf814aee611bea7f48f1479663406d
Callum McColl <callum.mccoll1@gmail.com>
2017-06-18 13:55:23 +1000

    Made FiniteStateMachine update the fsmVars

--- Sources/FSM/FiniteStateMachine.swift
+++ Sources/FSM/FiniteStateMachine.swift
@@ -281,7 +281,7 @@ public struct FiniteStateMachine<R: Ringlet, KR: KripkePropertiesRecorder, V: Va
     }
 
     public mutating func update(fromDictionary dictionary: [String: Any]) {
-        //self.fsmVars.vars.update(fromDictionary: dictionary["fsmVars"] as! [String: Any])
+        self.fsmVars.vars.update(fromDictionary: dictionary["fsmVars"] as! [String: Any])
         //self.currentState.update(fromDictionary: dictionary[self.currentState.name] as! [String: Any])
     }
 
