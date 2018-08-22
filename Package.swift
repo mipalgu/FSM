6eceea9a328e040292a900120581202e2605c513
Callum McColl <callum.mccoll1@gmail.com>
2018-08-22 10:05:55 +1000

    Make generate libFSM.dylib include all new targets

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(
             name: "FSM",
             type: .dynamic,
-            targets: ["Functional", "KripkeStructure", "FSM", "swiftfsm"]
+            targets: ["Utilities", "Functional", "KripkeStructure", "ModelChecking", "FSM", "ExternalVariables", "FSMVerification", "swiftfsm"]
         )
     ],
     dependencies: [
@@ -18,11 +18,11 @@ let package = Package(
         .target(name: "Utilities", dependencies: []),
         .target(name: "Functional", dependencies: []),
         .target(name: "KripkeStructure", dependencies: ["Functional", "Utilities"]),
-        .target(name: "ModelChecking", dependencies: ["GUSimpleWhiteboard", "Functional", "Utilities", "KripkeStructure"]),
-        .target(name: "FSM", dependencies: ["GUSimpleWhiteboard", "Functional", "Utilities", "KripkeStructure", "ModelChecking"]),
+        .target(name: "ModelChecking", dependencies: ["Functional", "Utilities", "KripkeStructure"]),
+        .target(name: "FSM", dependencies: ["Functional", "Utilities", "KripkeStructure", "ModelChecking"]),
         .target(name: "ExternalVariables", dependencies: ["GUSimpleWhiteboard", "Functional", "Utilities", "KripkeStructure", "ModelChecking", "FSM"]),
-        .target(name: "FSMVerification", dependencies: ["GUSimpleWhiteboard", "Functional", "Utilities", "KripkeStructure", "ModelChecking", "FSM"]),
-        .target(name: "swiftfsm", dependencies: ["GUSimpleWhiteboard", "Functional", "Utilities", "KripkeStructure", "FSM", "ModelChecking"]),
+        .target(name: "FSMVerification", dependencies: ["Functional", "Utilities", "KripkeStructure", "ModelChecking", "FSM"]),
+        .target(name: "swiftfsm", dependencies: ["Functional", "Utilities", "KripkeStructure", "FSM", "ModelChecking"]),
         .testTarget(name: "FSMTests", dependencies: [.target(name: "FSM"), .target(name: "swiftfsm")])
     ]
 )
