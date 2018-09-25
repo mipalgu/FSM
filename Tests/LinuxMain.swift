6eb7942648c91d023384bd34d463bd5d8ed09502
Callum McColl <callum.mccoll1@gmail.com>
2018-09-26 08:57:09 +1000

    Made tests run on linux again

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,9 +2,9 @@ import XCTest
 @testable import FSMTests
 
 XCTMain([
-     testCase(BehaviourTests.allTests),
      testCase(CArrayTests.allTests),
      testCase(FactoriesTests.allTests),
+     testCase(HashTableCycleDetectorTests.allTests),
+     testCase(MultipleExternalsSpinnerConstructorTests.allTests),
      //testCase(SpinnerTests.allTests),
-     testCase(WhiteboardBehaviourTests.allTests)
 ])
