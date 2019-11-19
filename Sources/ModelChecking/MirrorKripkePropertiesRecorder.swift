d20008cd8a91e6d072cbfd0c270abf9c7f84ad03
Callum McColl <callum.mccoll1@gmail.com>
2019-11-19 13:52:39 +1000

    Changed MirrorKripkePropertiesRecorder to cast to AnyObject instead of Anyclass

--- Sources/ModelChecking/MirrorKripkePropertiesRecorder.swift
+++ Sources/ModelChecking/MirrorKripkePropertiesRecorder.swift
@@ -318,7 +318,7 @@ public final class MirrorKripkePropertiesRecorder: KripkePropertiesRecorder {
             return (.String, val)
         default:
             var memoryCache = memoryCache
-            if let temp = val as? AnyClass {
+            if let temp = val as? AnyObject {
                 if nil != memoryCache.first(where: { $0 === temp }) {
                     return (.Compound(KripkeStatePropertyList()), val)
                 }
