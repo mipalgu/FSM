e8e6ed5d29c586e74dd4c46934c28fdb77d2ac34
Callum McColl <callum.mccoll1@gmail.com>
2022-08-11 10:25:23 +1000

    Added docC plugin dependency

--- Package.swift
+++ Package.swift
@@ -13,7 +13,9 @@ let package = Package(
             ]
         )
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    ],
     targets: [
         .target(name: "Utilities", dependencies: []),
         .target(name: "FSM", dependencies: ["Utilities"]),
